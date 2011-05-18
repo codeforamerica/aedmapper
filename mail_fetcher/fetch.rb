5218ab8221b9204a2cb4b659a7b7c84500e193e3
Max Ogden <max@maxogden.com>
2011-05-17 17:29:41 -0700

    add sample .aedmapperrc

--- mail_fetcher/fetch.rb
+++ mail_fetcher/fetch.rb
@@ -3,10 +3,14 @@ load 'imap_client.rb'
 load 'aed_response.rb'
 require 'uri'
 
-# export AED_COUCH_URL="http://localhost:5984/mail/"
+# sample ~/.aedmapperrc:
+# username: your@email.com
+# password: password
+# couch_url: http://localhost:5984/aedmapper/
+# sendgrid_user: your@sendgridemail.com
+# sendgrid_pass: sendgridpassword
 config = YAML::load File.read(File.expand_path('~/.aedmapperrc'))
 @couch_url = URI.parse(config['couch_url'])
-
 @imap = Gmail::ImapClient.new(config)
 @couch = Couch::Server.new(@couch_url.host, @couch_url.port)
 
@@ -18,7 +22,7 @@ config = YAML::load File.read(File.expand_path('~/.aedmapperrc'))
         doc = JSON.parse(@couch.get(@couch_url.path + message.gmail_plus_label).body)
         doc = {} if doc.has_key? 'error'
         doc.merge! message.attributes
-        @couch.put("/mail/" + doc['_id'], doc.to_json)
+        @couch.put(@couch_url.path + doc['_id'], doc.to_json)
       else
         doc = {'_id' => JSON.parse(@couch.post(@couch_url.path, message.attributes.to_json).body)['id']}
       end
